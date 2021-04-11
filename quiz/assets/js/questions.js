1587cd2b8a131bb75fff31bb0cff3dbb4b4ce118
apratt7891 <apratt7891@gmail.com>
2021-04-11 13:13:55 -0600

    add questions js and questions

---
+++ quiz/assets/js/questions.js
@@ -0,0 +1,35 @@
+var questions = [
+    {
+        q: "What is CSS",
+        o: {
+            a: "A rap band",
+            b: "the language we use to style a Web page",
+            c: "A web development site",
+            d: "ice cream flavor",
+        },
+        // b
+        a: "b"
+    },
+    {
+        q: "What does DOM stand for?",
+        o: {
+            a: "Doctor of Medicine",
+            b: "Dawn of madness",
+            c: "Document Object Model",
+            d: "Doodle on the moon",
+        },
+        // c
+        a:"c"
+    },
+    {
+        q: "The condition in an if/else statement is enclosed with ____.",
+        o: {
+            a: "quotes",
+            b: "curley brackets",
+            c: "parenthesis",
+            d: "square brackets",
+        },
+        //c
+        a:"c"
+    }
+];
