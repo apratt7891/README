a830a8d17977fcc058842787d1f57a48424bd4d6
apratt7891 <apratt7891@gmail.com>
2021-05-16 08:17:51 -0600

    add prompt

--- README-generator/index.js
+++ README-generator/index.js
@@ -1,3 +1,5 @@
+const inquirer = require('inquirer');
+console.log(inquirer)
 const fs = require('fs');
 
 const profileDataArgs = process.argv.slice(2);
@@ -28,3 +30,60 @@ fs.writeFile('./index.html', generatePage(name, github), err => {
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'Your Project Title',
+      message: 'What is the name of your project? (Required)',
+      validate: projectNameInput => {
+        if (projectNameInput) {
+          return true;
+        } else {
+          console.log('Enter the name of your project!');
+          return false;
+        }
+      }
+    },
+  {
+      type: 'input',
+      name: 'description',
+      message: 'Add a description of your project (Required)',
+      validate: (descriptionInput) => {
+        if(descriptionInput) {
+        return true;
+      } else {
+        console.log('Enter a description of your project');
+        return false;
+      }
+    }
+  },
+  {
+      type: 'input',
+      name: 'installation',
+      message: 'Add installation instructions for your project(Required)',
+      validate: (installationInput) => {
+        if(installationInput) {
+        return true;
+      } else {
+        console.log('Add installation instructions for your project');
+        return false;
+      } 
+    } 
+  },
+  {
+      type: 'input',
+      name: 'usage',
+      message: 'Add usage information for your project(Required)',
+      validate: (usageInput) => {
+        if(usageInput) {
+        return true;
+      } else {
+        console.log('Add usage information for your project');
+        return false;
+      }
+      }}])}
+
+promptUser().then(answers => console.log(answers));
+
